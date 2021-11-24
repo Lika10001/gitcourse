417bd22b9ae8d553fd45cb4882db7a57d57022f8
Maxin4rm <92393477+Maxin4rm@users.noreply.github.com>
2021-11-24 22:37:55 +0200

    Update Main.java

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -5,71 +5,87 @@ import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Main extends Application {
 
-    public static final int TILE_SIZE = 100;
-    public static final int WIDTH = 8;
-    public static final int HEIGHT = 8;
+    static final int TILE_SIZE = 100;
+    private static final int WIDTH = 8;
+    private static final int HEIGHT = 8;
 
     private Tile[][] board = new Tile[WIDTH][HEIGHT];
 
     private Group tileGroup = new Group();
     private Group pieceGroup = new Group();
+    private AtomicInteger counter = new AtomicInteger();
 
-
-
+    private Parent createMenu(){
+        Pane root2 = new Pane();
+        Button RestartButton = new Button();
+        RestartButton.setText("Restart");
+        RestartButton.setTranslateX(900);
+        RestartButton.setTranslateY(80);
+        return root2;
+    }
 
 
     private Parent createContent() {
         Pane root = new Pane();
         root.setPrefSize(WIDTH * TILE_SIZE, HEIGHT * TILE_SIZE);
 /*
-Button RestartButton = new Button();
-RestartButton.setText("Restart");
-RestartButton.setTranslateX(900);
-RestartButton.setTranslateY(80);
-RestartButton.setPrefSize(70,30);
+        Button RestartButton = new Button();
+        RestartButton.setText("menu");
+        RestartButton.setTranslateX(900);
+        RestartButton.setTranslateY(80);
+        RestartButton.setPrefSize(70,30);
 
-Button menuButton = new Button();
-menuButton.setText("Menu");
-menuButton.setTranslateX(900);
-menuButton.setTranslateY(30);
-menuButton.setPrefSize(70,30);
+        Button menuButton = new Button();
+        menuButton.setText("Menu");
+        menuButton.setTranslateX(900);
+        menuButton.setTranslateY(30);
+        menuButton.setPrefSize(70,30);
 
-menuButton.setOnMouseClicked(mouseEvent -> {
-root.getChildren().addAll(RestartButton);
-});
-RestartButton.setOnMouseClicked(mouseEvent -> {
+        menuButton.setOnMouseClicked(mouseEvent -> {
+            root.getChildren().addAll(RestartButton);
+        });
 
-for (int y = 0; y < HEIGHT; y++) {
-for (int x = 0; x < WIDTH; x++) {
-Tile tile = new Tile((x + y) % 2 == 0, x, y);
-board[x][y] = tile;
+        RestartButton.setOnMouseClicked(mouseEvent -> {
+            Stage stage = (Stage) RestartButton.getScene().getWindow();
+            stage.hide();
+            stage.show();
 
-tileGroup.getChildren().add(tile);
 
-Piece piece = null;
+            /*
+            for (int y = 0; y < HEIGHT; y++) {
+                for (int x = 0; x < WIDTH; x++) {
+                    Tile tile = new Tile((x + y) % 2 == 0, x, y);
+                    board[x][y] = tile;
 
-if (y <= 2 && (x + y) % 2 != 0) {
-piece = makePiece(PieceType.RED, x, y);
-}
+                    tileGroup.getChildren().add(tile);
 
-if (y >= 5 && (x + y) % 2 != 0) {
-piece = makePiece(PieceType.WHITE, x, y);
-}
+                    Piece piece = null;
 
-if (piece != null) {
-tile.setPiece(piece);
-pieceGroup.getChildren().add(piece);
-}
-}
-}
-} );
+                    if (y <= 2 && (x + y) % 2 != 0) {
+                        piece = makePiece(PieceType.RED, x, y);
+                    }
+
+                    if (y >= 5 && (x + y) % 2 != 0) {
+                        piece = makePiece(PieceType.WHITE, x, y);
+                    }
+
+                    if (piece != null) {
+                        tile.setPiece(piece);
+                        pieceGroup.getChildren().add(piece);
+                    }
+                }
+           }
+        } );
 */
-        root.getChildren().addAll(tileGroup, pieceGroup/*, menuButton*/);
+        root.getChildren().addAll(tileGroup, pieceGroup);
 
         for (int y = 0; y < HEIGHT; y++) {
             for (int x = 0; x < WIDTH; x++) {
@@ -101,29 +117,53 @@ pieceGroup.getChildren().add(piece);
         if (board[newX][newY].hasPiece() || (newX + newY) % 2 == 0) {
             return new MoveResult(MoveType.NONE);
         }
-
         int x0 = toBoard(piece.getOldX());
         int y0 = toBoard(piece.getOldY());
+        if (counter.get() % 2 == 0) {
+            if (Math.abs(newX - x0) == 1 && newY - y0 == -1) {
+                counter.getAndIncrement();
+                return new MoveResult(MoveType.NORMAL);
+            } else if (Math.abs(newX - x0) == 2 && newY - y0 == -2) {
 
-        if (Math.abs(newX - x0) == 1 && newY - y0 == piece.getType().moveDir) {
-            return new MoveResult(MoveType.NORMAL);
-        } else if (Math.abs(newX - x0) == 2 && newY - y0 == piece.getType().moveDir * 2) {
+                int x1 = x0 + (newX - x0) / 2;
+                int y1 = y0 + (newY - y0) / 2;
 
-            int x1 = x0 + (newX - x0) / 2;
-            int y1 = y0 + (newY - y0) / 2;
+                if (board[x1][y1].hasPiece() && board[x1][y1].getPiece().getType() != piece.getType()) {
+                    counter.getAndIncrement();
+                    return new MoveResult(MoveType.KILL, board[x1][y1].getPiece());
+                }
+            } else if (Math.abs(newX - x0) == 2 && y0 - newY == -2) {//можно упростить?
+                int x1 = x0 + (newX - x0) / 2;
+                int y1 = y0 + (newY - y0) / 2;
 
-            if (board[x1][y1].hasPiece() && board[x1][y1].getPiece().getType() != piece.getType()) {
-                return new MoveResult(MoveType.KILL, board[x1][y1].getPiece());
+                if (board[x1][y1].hasPiece() && board[x1][y1].getPiece().getType() != piece.getType()) {
+                    counter.getAndIncrement();
+                    return new MoveResult(MoveType.KILL, board[x1][y1].getPiece());
+                }
             }
-        } else if(Math.abs(newX - x0) == 2 && y0 - newY == piece.getType().moveDir * 2){//можно упростить?
-            int x1 = x0 + (newX - x0) / 2;
-            int y1 = y0 + (newY - y0) / 2;
+        } else {
+            if (Math.abs(newX - x0) == 1 && newY - y0 == 1) {
+                counter.getAndIncrement();
+                return new MoveResult(MoveType.NORMAL);
+            } else if (Math.abs(newX - x0) == 2 && newY - y0 == 2) {
+                int x1 = x0 + (newX - x0) / 2;
+                int y1 = y0 + (newY - y0) / 2;
+                if (board[x1][y1].hasPiece() && board[x1][y1].getPiece().getType() != piece.getType()) {
+                    counter.getAndIncrement();
+                    return new MoveResult(MoveType.KILL, board[x1][y1].getPiece());
+                }
+            } else if (Math.abs(newX - x0) == 2 && y0 - newY == 2) {//можно упростить?
+                int x1 = x0 + (newX - x0) / 2;
+                int y1 = y0 + (newY - y0) / 2;
 
-            if (board[x1][y1].hasPiece() && board[x1][y1].getPiece().getType() != piece.getType()) {
-                return new MoveResult(MoveType.KILL, board[x1][y1].getPiece());
+                if (board[x1][y1].hasPiece() && board[x1][y1].getPiece().getType() != piece.getType()) {
+                    counter.getAndIncrement();
+                    return new MoveResult(MoveType.KILL, board[x1][y1].getPiece());
+                }
             }
         }
 
+        System.out.println(counter.get());
         return new MoveResult(MoveType.NONE);
     }
 
@@ -133,13 +173,20 @@ pieceGroup.getChildren().add(piece);
 
     @Override
     public void start(Stage primaryStage) {
-        int counter = 0;
         Scene scene = new Scene(createContent());
+        Scene scene2 = new Scene(createMenu());
         primaryStage.setTitle("Шашки");
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
         primaryStage.setWidth(1000);
         primaryStage.show();
+        scene.setOnKeyPressed(event -> {
+            if (event.getCode() == KeyCode.ESCAPE){
+                System.out.println("Hello World");
+
+
+            }
+        });
     }
 
     private Piece makePiece(PieceType type, int x, int y) {
@@ -157,8 +204,7 @@ pieceGroup.getChildren().add(piece);
                 result = tryMove(piece, newX, newY);
             }
 
-            int x0 =
-                    toBoard(piece.getOldX());
+            int x0 = toBoard(piece.getOldX());
             int y0 = toBoard(piece.getOldY());
 
             switch (result.getType()) {
