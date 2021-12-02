c7fd596af6c999163fbdf602423d4558f3cf78e6
Pe4enka1606 <92690471+Pe4enka1606@users.noreply.github.com>
2021-12-02 11:53:30 +0300

    Update ChangeType.java

--- src/main/java/sample/ChangeType.java
+++ src/main/java/sample/ChangeType.java
@@ -5,18 +5,111 @@ public class ChangeType {
     public static String inputPathToFile() {
         boolean isInCorrect;
         String path = "in.txt";
-        System.out.println("Введите ссылку на файл.");
         return path;
     }
-
-    public static int[] takeProduct(int sizeOfVector, int linesOfMatrix, int columnsOfMatrix, int[] vector, int[][] matrix) {
-        int[] prod = new int[sizeOfVector];
-        for (int i = 0; i < linesOfMatrix; i++) {
-            prod[i] = 0;
-            for (int j = 0; j < columnsOfMatrix; j++) {
-                prod[i] = prod[i] + matrix[i][j] * vector[i];
-            }
-        }
-        return prod;
+    public static int calculate1(){
+        int actual;
+        int a = 6;
+        int b = 10;
+        int res = a + b;
+        return res;
+    }
+    public static int calculate2(){
+        int actual;
+        int a = 7;
+        int b = 11;
+        int res = a + b;
+        return res;
+    }
+    public static int calculate3(){
+        int actual;
+        int a = 8;
+        int b = 12;
+        int res = a + b;
+        return res;
+    }
+    public static int calculate4(){
+        int actual;
+        int a = 9;
+        int b = 13;
+        int res = a + b;
+        return res;
+    }
+    public static int calculate5(){
+        int actual;
+        int a = 10;
+        int b = 14;
+        int res = a + b;
+        return res;
+    }
+    public static int calculate6(){
+        int actual;
+        int a = 11;
+        int b = 15;
+        int res = a + b;
+        return res;
+    }
+    public static int calculate7(){
+        int actual;
+        int a = 12;
+        int b = 16;
+        int res = a + b;
+        return res;
+    }
+    public static int calculate8(){
+        int actual;
+        int a = 13;
+        int b = 17;
+        int res = a + b;
+        return res;
+    }
+    public static int calculate9(){
+        int actual;
+        int a = 14;
+        int b = 18;
+        int res = a + b;
+        return res;
+    }
+    public static int calculate10(){
+        int actual;
+        int a = 15;
+        int b = 19;
+        int res = a + b;
+        return res;
+    }
+    public static int calculate11(){
+        int actual;
+        int a = 16;
+        int b = 20;
+        int res = a + b;
+        return res;
+    }
+    public static int calculate12(){
+        int actual;
+        int a = 17;
+        int b = 21;
+        int res = a + b;
+        return res;
+    }
+    public static int calculate13(){
+        int actual;
+        int a = 18;
+        int b = 22;
+        int res = a + b;
+        return res;
+    }
+    public static int calculate14(){
+        int actual;
+        int a = 19;
+        int b = 23;
+        int res = a + b;
+        return res;
+    }
+    public static int calculate15(){
+        int actual;
+        int a = 20;
+        int b = 24;
+        int res = a + b;
+        return res;
     }
 }
